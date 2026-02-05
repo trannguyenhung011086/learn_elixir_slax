c8ec68dd7a90be5b143fe66d7bd9732fe5888391
Hung Tran <trannguyenhung011086@gmail.com>
2026-02-05 23:42:21 +0700

    add toggle lists with LiveView.JS

--- lib/slax_web/components/core_components.ex
+++ lib/slax_web/components/core_components.ex
@@ -438,10 +438,12 @@ defmodule SlaxWeb.CoreComponents do
   """
   attr :name, :string, required: true
   attr :class, :any, default: "size-4"
+  attr :id, :string, default: nil
+  attr :style, :string, default: nil
 
   def icon(%{name: "hero-" <> _} = assigns) do
     ~H"""
-    <span class={[@name, @class]} />
+    <span class={[@name, @class]} id={@id} style={@style} />
     """
   end
 
--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -21,18 +21,12 @@ defmodule SlaxWeb.ChatRoomLive do
           </div>
         </div>
         <div class="mt-4 overflow-auto">
-          <div class="flex items-center h-8 px-3">
-            <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
-          </div>
+          <.toggler on_click={toggle_rooms()} dom_id="rooms-toggler" text="Rooms" />
           <div id="rooms-list">
             <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
           </div>
           <div class="mt-4">
-            <div class="flex items-center h-8 px-3">
-              <div class="flex items-center grow">
-                <span class="ml-2 leading-none font-medium text-sm">Users</span>
-              </div>
-            </div>
+            <.toggler on_click={toggle_users()} dom_id="users-toggler" text="Users" />
             <div id="users-list">
               <.user
                 :for={user <- @users}
@@ -117,6 +111,41 @@ defmodule SlaxWeb.ChatRoomLive do
       </div>
     </Layouts.app>
     """
+  end
+
+  attr :dom_id, :string, required: true
+  attr :on_click, JS, required: true
+  attr :text, :string, required: true
+
+  defp toggler(assigns) do
+    ~H"""
+    <div class="flex items-center h-8 px-3">
+      <button id={@dom_id} phx-click={@on_click} class="flex items-center grow">
+        <.icon id={@dom_id <> "-chevron-down"} name="hero-chevron-down" class="h-4 w-4" />
+        <.icon
+          id={@dom_id <> "-chevron-right"}
+          name="hero-chevron-right"
+          class="h-4 w-4"
+          style="display:none;"
+        />
+        <span class="ml-2 leading-none font-medium text-sm">
+          {@text}
+        </span>
+      </button>
+    </div>
+    """
+  end
+
+  defp toggle_rooms do
+    JS.toggle(to: "#rooms-toggler-chevron-down")
+    |> JS.toggle(to: "#rooms-toggler-chevron-right")
+    |> JS.toggle(to: "#rooms-list")
+  end
+
+  defp toggle_users do
+    JS.toggle(to: "#users-toggler-chevron-down")
+    |> JS.toggle(to: "#users-toggler-chevron-right")
+    |> JS.toggle(to: "#users-list")
   end
 
   attr :current_user, Slax.Accounts.User, required: true
